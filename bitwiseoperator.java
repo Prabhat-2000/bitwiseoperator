ad1c3ec6374a23c557420e7df3b4a3a0b08bb8e7
Prabhat-2000 <102291706+Prabhat-2000@users.noreply.github.com>
2022-07-29 22:23:43 +0530

    Add files via upload

---
+++ bitwiseoperator.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class bitwiseoperator {
+    public static void main( String[] args){
+        int a=5,b=7;
+        System.out.println(a & b);
+        System.out.println(a | b);
+    }
+}
